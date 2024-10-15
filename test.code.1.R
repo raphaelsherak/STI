a121aa733ed815c592fd162a74bf22b686439f48
RSherak <rsherak@gmail.com>
2024-10-15 09:43:40 -0400

    old changes

--- test.code.1.R
+++ test.code.1.R
@@ -1,44 +1,4 @@
-library(tidyverse)
-library(readr)
-library(readxl)
-> encounters <- read_excel("~/Documents/Current_Projects/STI/STI_JDAT_March1.xlsx", 
-                           +     sheet = "ED_ENC_and_DEM", col_types = c("text", "text", "date", "numeric", "text", "text", "text", "date", "text", "numeric", 
-                                                                         +         "numeric", "numeric", "text", "text", 
-                                                                         +         "text", "text", "text", "numeric", 
-                                                                         +         "numeric", "date", "text", "text", 
-                                                                         +         "text", "text", "text"))
-
-encounters <-
-  read_excel(
-    "~/Documents/Current_Projects/STI/STI_JDAT_March1.xlsx",
-    sheet = "ED_ENC_and_DEM",
-    col_types = c(
-      "text",
-      "text",
-      "date",
-      "numeric",
-      "text",
-      "text",
-      "text",
-      "date",
-      "text",
-      "numeric",
-      "numeric",
-      "numeric",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text",
-      "numeric",
-      "numeric",
-      "date",
-      "text",
-      "text",
-      "text",
-      "text",
-      "text"
-    )
-  )
-print(colnames(encounters))
-encounters %>% summarise(patients = n_distinct(PAT_MRN_ID), visits = n_distinct(PAT_ENC_CSN_ID), earliest = min(ED_ADMISSION_TIME), latest = max(ED_ADMISSION_TIME))
+query <- encounters %>% select(PAT_MRN_ID, PAT_ENC_CSN_ID, ED_DISPOSITION, ARRIVAL_LOCATION)
+discharged<- query %>% filter(ED_DISPOSITION == "Discharge")
+query2<- STI_JDAT_March1 %>% select(PAT_MRN_ID,PAT_ENC_CSN_ID, Order_Date, Med_ShortName)
+doxyrx<- query2 %>% filter(str_detect(pattern = "doxy", string = Med_ShortName))
