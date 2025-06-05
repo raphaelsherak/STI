b5221037e1ab25769bff3739ea745e8fee16cf75
RSherak <43275652+raphaelsherak@users.noreply.github.com>
2025-06-05 17:18:03 -0400

    Delete test.code.1.R

--- test.code.1.R
+++
@@ -1,4 +0,0 @@
-query <- encounters %>% select(PAT_MRN_ID, PAT_ENC_CSN_ID, ED_DISPOSITION, ARRIVAL_LOCATION)
-discharged<- query %>% filter(ED_DISPOSITION == "Discharge")
-query2<- STI_JDAT_March1 %>% select(PAT_MRN_ID,PAT_ENC_CSN_ID, Order_Date, Med_ShortName)
-doxyrx<- query2 %>% filter(str_detect(pattern = "doxy", string = Med_ShortName))
